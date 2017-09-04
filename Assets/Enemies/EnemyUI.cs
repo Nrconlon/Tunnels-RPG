0f097105d2e01a44c9e3cae8a7843bf04c4aae2b
Neil Conlon <nrconlon@gmail.com>
2017-09-04 13:45:05 -0700

    Added Health Bars to player and enemy

---
+++ Assets/Enemies/EnemyUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+// Add a UI Socket transform to your enemy
+// Attach this script to the socket
+// Link to a canvas prefab that contains NPC UI
+public class EnemyUI : MonoBehaviour {
+
+    // Works around Unity 5.5's lack of nested prefabs
+    [Tooltip("The UI canvas prefab")]
+
+    Camera cameraToLookAt;
+
+    // Use this for initialization 
+    void Start()
+    {
+        cameraToLookAt = Camera.main;
+    }
+
+    // Update is called once per frame 
+    void LateUpdate()
+    {
+        transform.LookAt(cameraToLookAt.transform);
+        transform.rotation = Quaternion.LookRotation(cameraToLookAt.transform.forward);
+    }
+}
