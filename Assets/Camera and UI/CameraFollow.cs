a54a174532ebbea972dcb7e66435ad6c5e15a411
Neil Conlon <nrconlon@gmail.com>
2017-09-04 11:05:04 -0700

    All Files Initialized

---
+++ Assets/Camera and UI/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");  //Player is the tag we gave to player in unity
+		print(player.ToString());
+	}
+	
+	// LateUpdate is called once per frame, after everything else is calculated (including character position)
+	void LateUpdate () {
+		transform.position = player.transform.position;
+	}
+}
---
+++ Assets/Player/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField]  float maxHealthPoints = 100f;
+
+	float currentHealthPoints = 1;
+
+	private void Start()
+	{
+		currentHealthPoints = maxHealthPoints;
+	}
+	public float healthAsPercentage
+	{
+		get
+		{
+			return currentHealthPoints / maxHealthPoints;
+		}
+
+	}
+
+}
