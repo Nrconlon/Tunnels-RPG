44257ea4522a919c62b55d9e5145d358219464c3
Neil Conlon <nrconlon@gmail.com>
2017-09-12 07:21:46 -0700

    tailJumpBroken

--- Assets/Characters/Molat.cs
+++ Assets/Characters/Molat.cs
@@ -1,39 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class Molat : MonoBehaviour, IDamageable
 {
 
-
-	[SerializeField] float maxStamina = 100f;
+	[Header("Stats")]
 	[SerializeField] float maxHealth = 100f;
+	[SerializeField] float damage = 50f;
+	[SerializeField] float maxStamina = 100f;
+	[SerializeField] float stamRechargePS = 20f;
+
+	[Header("Movement")]
+	[SerializeField] float speed = 6f;
+	[SerializeField] float straifSpeed = 5f;
+	[SerializeField] float sprintSpeed = 8f;
+	[Range(0f, 180f)] [SerializeField] float fullSpeedMaxDegrees = 80f;
+	[SerializeField] float maxVelocityChange = 10.0f;
+	[SerializeField] float climbSpeed = 8f;
+
+	[Header("Abilities")]
 	[SerializeField] bool canJump = true;
 	[SerializeField] bool canAttack = true;
 	[SerializeField] bool canSprint = true;
 	[SerializeField] bool canClimb = false;
-	[SerializeField] float speed = 6f;
-	[SerializeField] float sprintSpeed = 8f;
-	//[SerializeField] float climbSpeed = 8f;
 
-	[SerializeField] float stamRechargePS = 20f;
+	[Header("Jumping")]
 	[SerializeField] float sprintCostPS = 5f;
-	//[SerializeField] float climbCostPS = 5f;
+	[SerializeField] float climbCostPS = 5f;
 	[SerializeField] float tailJumpCost = 5f;
 	[SerializeField] float tailJumpHeight = 3.0f;
 	[SerializeField] float tailJumpPower = 1000.0f;
-
-	[SerializeField] float gravity = 18;
 	[SerializeField] float jumpCost = 10f;
 	[SerializeField] float jumpHeight = 13.0f;
 	[SerializeField] float jumpPower = 2.0f;
-
-	[SerializeField] float maxVelocityChange = 10.0f;
-	[SerializeField] float dampTime = 2;
-	[SerializeField] float runAnimationRatio = 0.3f;
 	[SerializeField] LayerMask mask;
 	[SerializeField] float downCastRange = 1.2f;
 
+	[Header("Other")]
+	[SerializeField] float gravity = 18;
+	[SerializeField] float animDampTime = 2;
+	[SerializeField] float runAnimationRatio = 0.3f;
 	[SerializeField] float damageWithMaceThrow = 9.0f;
 	public GameObject throwingWeapon;
 	[SerializeField] GameObject projectileSocket;
@@ -41,6 +49,7 @@ public class Molat : MonoBehaviour, IDamageable
 
 	private float currentStamRechargePS;
 	private float currentSpeed;
+	private bool currentCanSprint;
 	private bool isJumping;
 	private bool isSprinting;
 	private bool isClimbing;
@@ -51,11 +60,11 @@ public class Molat : MonoBehaviour, IDamageable
 
 	private bool weaponEquiped = false;
 	private bool changingWeapon = false;
-
-
-
-	[SerializeField] AudioSource myaudiosource;
 	//[SerializeField] Transform target;
+
+
+	[Header("Body")]
+	[SerializeField] AudioSource myaudiosource;
 	[SerializeField] Transform chest;
 	[SerializeField] Transform shield;
 	[SerializeField] Transform weapon;
@@ -70,7 +79,7 @@ public class Molat : MonoBehaviour, IDamageable
 	[SerializeField] AudioClip holster1sound;
 	[SerializeField] AudioClip holster2sound;
 
-	[HideInInspector]  public Vector3 targetDirection;
+	[HideInInspector] public Vector3 targetDirection;
 	[HideInInspector] public Vector3 lookAtDirection;
 
 	private bool grounded;
@@ -105,6 +114,7 @@ public class Molat : MonoBehaviour, IDamageable
 		isSprinting = false;
 		isClimbing = false;
 		isBlocking = false;
+		currentCanSprint = true;
 		canAttackCurrent = canAttack;
 		grounded = true;
 	}
@@ -120,16 +130,29 @@ public class Molat : MonoBehaviour, IDamageable
 
 			if (targetDirection != Vector3.zero)
 			{
-				Vector3 velocity = m_RigidBody.velocity;
-				
 
-				
+
+				if (Mathf.Abs(AngleOfLookDirection()) > fullSpeedMaxDegrees)
+				{
+					currentSpeed = straifSpeed;
+					currentCanSprint = false;
+					isSprinting = false;
+				}
+				else if (isSprinting)
+				{
+					currentSpeed = sprintSpeed;
+					currentCanSprint = true;
+				}
+				else
+				{
+					currentSpeed = speed;
+					currentCanSprint = true;
+				}
 
 				Vector3 targetVelocity = targetDirection;
 				targetVelocity *= currentSpeed;
 
-
-				Vector3 velocityChange = (targetVelocity - velocity);
+				Vector3 velocityChange = (targetVelocity - m_RigidBody.velocity);
 				velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
 				velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
 				velocityChange.y = 0;
@@ -140,12 +163,12 @@ public class Molat : MonoBehaviour, IDamageable
 				Vector3 currentmagnitude = new Vector3(x, 0, z);
 				float velocityanim = Mathf.Clamp01(currentmagnitude.magnitude) * 2; //Take out times 2 when walking is added, as well as edit anim speeds (straife is too fast for walk)+
 
-				animator.SetFloat("speed", velocityanim, dampTime, 0.2f);
-				animator.SetFloat("movementAngle", AngleOfLookDirection(), dampTime, 0.2f);
+				animator.SetFloat("speed", velocityanim, animDampTime, 0.2f);
+				animator.SetFloat("movementAngle", AngleOfLookDirection(), animDampTime, 0.2f);
 			}
 			else
 			{
-				animator.SetFloat("speed", 0, dampTime * 2, 0.2f);
+				animator.SetFloat("speed", 0, animDampTime * 2, 0.2f);
 				animator.SetFloat("movementAngle", 0);
 			}
 		}
@@ -233,45 +256,34 @@ public class Molat : MonoBehaviour, IDamageable
 	//Called per frame from controller
 	public bool TailSprint(bool toggle, Vector3 direction)
 	{
-		//Called on Tick
-		if(canSprint && freeToMove())
+		if (toggle && canSprint && freeToMove())
 		{
-			if (toggle)
+			//When out of stamina, update() forces us to stop sprinting.
+			//if we are not sprinting, we need enough for a jump to start again
+			if (!isSprinting && currentStamina > tailJumpCost)
 			{
-				//When out of stamina, update() forces us to stop sprinting.
-				//if we are not sprinting, we need enough for a jump to start again
-				if (!isSprinting && currentStamina > tailJumpCost)
-				{
-					//We only come in here once.  This is an action.
-					isSprinting = true;
+				//We only come in here once.  This is an action.
+				isSprinting = true;
 
-					//tailJump
-					myaudiosource.clip = jumpclip2;
-					myaudiosource.loop = false;
-					myaudiosource.pitch = 1;
-					myaudiosource.Play();
-					m_RigidBody.AddForce(direction.x * tailJumpPower, CalculateJumpVerticalSpeed(tailJumpHeight), direction.z * tailJumpPower);
-
-
-					currentSpeed = sprintSpeed;
-
-				}
-				else if(isSprinting && currentStamina > sprintCostPS)
-				{
-					currentSpeed = sprintSpeed;
-					isSprinting = true;
-				}
-				else
-				{
-					currentSpeed = speed;
-					isSprinting = false;
-				}
+				//tailJump
+				myaudiosource.clip = jumpclip2;
+				myaudiosource.loop = false;
+				myaudiosource.pitch = 1;
+				myaudiosource.Play();
+				m_RigidBody.AddForce(direction.x * tailJumpPower, CalculateJumpVerticalSpeed(tailJumpHeight), direction.z * tailJumpPower);
+			}
+			else if (isSprinting && currentStamina > sprintCostPS && AngleOfLookDirection() < fullSpeedMaxDegrees)
+			{
+				isSprinting = true;
 			}
 			else
 			{
 				isSprinting = false;
-				currentSpeed = speed;
 			}
+		}
+		else
+		{
+			isSprinting = false;
 		}
 		return isSprinting;
 	}
